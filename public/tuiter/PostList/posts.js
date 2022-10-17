574730b5717fdf8ae983e6552a9e5b6f744c7c02
JiarongF <jiaronf1@uci.edu>
2022-10-16 18:58:31 -0700

    finished explore screen and home screen

---
+++ public/tuiter/PostList/posts.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    userAvatar: "../../images/elonmusk.png",
+    userName: "Elon Musk",
+    userHandle: "elonmusk",
+    time: "23h",
+    image: "../../images/inspirationmission.png",
+    title: "Amazing show about @Inspiration4x mission",
+    highlight: "@Inspiration4x",
+    content : "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 " +
+        "crew on the first all-civilian orbital space...",
+    link: "https://www.netflix.com/",
+    linkName: "netflix.com",
+    reply: "4.2K",
+    retweet: "3.5K",
+    like: "37.5K",
+  },
+  {
+    userAvatar: "../../images/nypost.png",
+    userName: "New York Post",
+    userHandle: "nypost",
+    time: "23h",
+    image: "../../images/nypostImg.png",
+    title: "Grimes seen reading Karl Marx following split with world's richest man Elon Musk trib.al/nx2Gfaq",
+    highlight: "trib.al/nx2Gfaq",
+    content : "",
+    link: "",
+    linkName: "",
+    reply: "703",
+    retweet: "1,689",
+    like: "3,764",
+  },
+
+]
